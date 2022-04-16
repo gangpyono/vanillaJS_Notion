567d20daf6eadf5045a5a4786d2c45e9978a5a33
gangpyono <>
2022-04-16 22:33:04 +0900

    feat/ document get,post,delete, + detail api

---
+++ src/utils/api/documentApi.js
@@ -0,0 +1,41 @@
+import { request } from "./index.js";
+
+export const getDocumentDB = async () => {
+  const res = await request("/documents");
+
+  return res;
+};
+
+export const addDocumentDB = async (title, parent) => {
+  try {
+    const res = await request("/documents", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        parent,
+      }),
+    });
+
+    res.documents = [];
+    return res;
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const deleteDocumentDB = async (documentId) => {
+  const res = await request(`/documents/${documentId}`, {
+    method: "DELETE",
+  });
+  return;
+};
+
+export const documentDetailDB = async (id) => {
+  try {
+    const res = await request(`/documents/${id}`);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
