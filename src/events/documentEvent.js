b7152bde3a14a976260c769dfc817f88457447bb
gangpyono <>
2022-04-16 22:34:43 +0900

    feat/document customEvent

---
+++ src/events/documentEvent.js
@@ -0,0 +1,35 @@
+export const selectedEvent = async ($target, id) => {
+  const event = new CustomEvent("selected", {
+    bubbles: true,
+    detail: { id },
+  });
+
+  $target.dispatchEvent(event);
+};
+
+export const addEvent = async ($target, id) => {
+  const event = new CustomEvent("add", {
+    bubbles: true,
+    detail: { id },
+  });
+
+  $target.dispatchEvent(event);
+};
+
+export const deleteEvent = async ($target, id) => {
+  const event = new CustomEvent("delete", {
+    bubbles: true,
+    detail: { id },
+  });
+
+  $target.dispatchEvent(event);
+};
+
+export const updateEvent = async ($target, id) => {
+  const event = new CustomEvent("delete", {
+    bubbles: true,
+    detail: { id },
+  });
+
+  $target.dispatchEvent(event);
+};
