c4d43b5a67ddab66d877c916af75bd24a08256bf
gangpyono <>
2022-04-17 02:09:08 +0900

    Refactor/ document생성 모듈로 전환

--- src/module/documentModule.js
+++ src/module/documentModule.js
@@ -1,3 +1,19 @@
+export const createDocument = ({ id, title, documents }) => {
+  return `
+      <li data-id = ${id}>
+      <span class = "title">${title}<span>
+      <div>
+        <button class = "addBtn">+</button>
+        <button class = "deleteBtn">-</button>
+      </div>
+        ${
+          documents.length > 0
+            ? `<ul>${documents.map((document) => createDocument(document)).join("")}</ul>`
+            : ""
+        }
+      </li>`;
+};
+
 export const addDocument = (initialState, targetId, createdDocument) => {
   const nextState = JSON.parse(JSON.stringify(initialState));
 
@@ -32,6 +48,30 @@ export const deleteDocument = (initialState, targetId) => {
           const idx = parrentDocument.findIndex((document) => document.id === +targetId);
           parrentDocument.splice(idx, 1);
           nextState.push(...temp);
+          return;
+        }
+
+        recur(childDocument.documents);
+      }
+    }
+  };
+
+  recur(nextState);
+
+  return nextState;
+};
+
+export const updateDocument = (initialState, { id, title, content }) => {
+  const nextState = JSON.parse(JSON.stringify(initialState));
+
+  const recur = (parentDocument) => {
+    if (parentDocument.length === 0) {
+      return;
+    } else {
+      for (const childDocument of parentDocument) {
+        if (childDocument.id === +id) {
+          childDocument.title = title;
+          childDocument.content = content;
           return;
         }
 
