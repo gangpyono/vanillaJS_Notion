de7945470843791474fef0ce407f79403e6e64a4
gangpyono <>
2022-04-17 22:28:19 +0900

    refactor/ 이벤트구독으로 상태관리 변경

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,35 +4,54 @@ import { selectedEvent, addEvent, deleteEvent } from "../events/documentEvent.js
 //module
 import { createDocument } from "../module/documentModule.js";
 
-export default function Banner({ $target, initialState }) {
+export default function Banner({ $target }) {
+  const $ul = document.createElement("ul");
+  $ul.setAttribute("class", "itemList");
+  $target.appendChild($ul);
+
+  let state = {
+    documentList: [],
+  };
+
+  const setState = (nextState) => {
+    state = nextState;
+    render();
+  };
+
+  $ul.addEventListener("click", (e) => {
+    const $li = e.target.closest("li");
+    const id = $li.dataset.id;
+
+    const className = e.target.className;
+
+    if (className === "title") {
+      history.pushState(null, null, `/documentDetail/${id}`);
+      selectedEvent($target, id);
+      return;
+    }
+
+    if (className === "addBtn") {
+      addEvent($target, id);
+      return;
+    }
+
+    if (className === "deleteBtn") {
+      deleteEvent($target, id);
+      return;
+    }
+  });
+
   const render = () => {
-    $target.innerHTML = `<ul class = "itemList">${initialState
+    $ul.innerHTML = `${state.documentList
       .map((document) => {
         return createDocument(document);
       })
-      .join("")}</ul>`;
-
-    const $ul = document.querySelector(".itemList");
-
-    $ul.addEventListener("click", (e) => {
-      const className = e.target.className;
-      const $li = e.target.closest("li");
-      const id = $li.dataset.id;
-
-      if (className === "title") {
-        history.pushState(null, null, `/documentDetail/${id}`);
-        selectedEvent($target, id);
-      }
-
-      if (className === "addBtn") {
-        addEvent($target, id);
-      }
-
-      if (className === "deleteBtn") {
-        deleteEvent($target, id);
-      }
-    });
+      .join("")}`;
   };
 
-  render();
+  //init
+  $target.addEventListener("banner", (e) => {
+    const { documentList } = e.detail;
+    setState({ ...state, documentList });
+  });
 }
