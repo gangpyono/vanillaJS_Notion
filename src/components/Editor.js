79d70dda6e0ad983122ade5180e40d7a2a42921f
gangpyono <>
2022-04-17 22:28:43 +0900

    refactor/ 이벤트구독으로 상태관리 변경 + 입력 디바운스 처리.

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,31 +1,69 @@
 //event
 import { updateEvent } from "../events/documentEvent.js";
 
-export default function Editor({ $target, initialState: { id, title, content } }) {
+export default function Editor({ $target }) {
   const $editContainer = document.createElement("div");
-  console.log(1);
   $target.appendChild($editContainer);
 
-  const render = () => {
-    console.log(1);
-    $editContainer.innerHTML = `
-    <input class = "titleInput" value="${title}" autofocus />
-    <textarea class = "contentInput">${content}</textarea>
-    `;
+  let state = {
+    init: false,
+    documentDetail: {},
+  };
 
-    const $titleInput = document.querySelector(".titleInput");
-    const $contentInput = document.querySelector(".contentInput");
+  const setState = (nextState) => {
+    state = nextState;
+    render();
+  };
 
-    $editContainer.addEventListener("keyup", () => {
-      const document = {
-        id: id,
+  let timerID = null;
+
+  $editContainer.addEventListener("keyup", () => {
+    if (timerID !== null) {
+      clearTimeout(timerID);
+    }
+
+    timerID = setTimeout(() => {
+      const $titleInput = document.querySelector(".titleInput");
+      const $contentInput = document.querySelector(".contentInput");
+
+      const updatedDocument = {
+        id: state.documentDetail.id,
         title: $titleInput.value,
         content: $contentInput.value,
       };
 
-      updateEvent($target, document);
-    });
+      updateEvent($target, updatedDocument);
+    }, 1000);
+  });
+
+  const render = () => {
+    if (!state.init) {
+      $editContainer.innerHTML = `
+    <input class = "titleInput" value="${state.documentDetail.title}" autofocus/>
+    <textarea class = "contentInput">${state.documentDetail.content}</textarea>
+    `;
+
+      state.init = true;
+    }
   };
 
-  render();
+  // 렌더링 조건이 state가 변경되었을때 발생하기.
+  // init
+  $target.addEventListener("editor", (e) => {
+    const { documentDetail, isEditing } = e.detail;
+
+    // 루트경로검사
+    if (documentDetail === null) {
+      $editContainer.innerHTML = "";
+      return;
+    }
+
+    if (isEditing) {
+      setState({ ...state, init: true, documentDetail });
+      return;
+    }
+
+    setState({ ...state, init: false, documentDetail });
+    return;
+  });
 }
