fe5057ab14133560e7e800f39334ecb4296ea2d2
gangpyono <>
2022-04-19 17:02:29 +0900

    feat/ 루트폴더 추가버튼 컴포넌트

---
+++ src/components/RootAddBtn.js
@@ -0,0 +1,18 @@
+import { addEvent } from "../events/documentEvent.js";
+
+export default function RootAddBtn({ $target, $eventTarget }) {
+  const $button = document.createElement("button");
+
+  $button.setAttribute("class", "rootAddBtn");
+  $target.appendChild($button);
+
+  const render = () => {
+    $button.innerText = `+ Add a page`;
+  };
+
+  $button.addEventListener("click", () => {
+    addEvent($eventTarget, null);
+  });
+
+  render();
+}
