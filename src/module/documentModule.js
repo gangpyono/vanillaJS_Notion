157d8eae7465a24a9e0cc2b19eab7b3f29e8190b
gangpyono <>
2022-04-19 17:03:31 +0900

    feat/ 토글기능

--- src/module/documentModule.js
+++ src/module/documentModule.js
@@ -1,21 +1,32 @@
-export const createDocument = ({ id, title, documents }) => {
+export const createDocument = ({ id, title, active = false, documents }) => {
   return `
-      <li data-id = ${id}>
-      <span class = "title">${title}<span>
-      <div>
-        <button class = "addBtn">+</button>
-        <button class = "deleteBtn">-</button>
+      <li data-id = ${id} class = "bannerItem">
+      <div class = "itemInner">
+        <div class = "itemInner_left">
+          <button class = "btn toggleBtn">${active ? "▼" : "▶"}</button>
+          <span class = "itemTitle">${title === "" ? "untitle" : title}</span>
+        </div>  
+        <div class = "btnGroup">
+          <button class = "btn deleteBtn">−</button>
+          <button class = "btn addBtn">+</button>
+        </div>
       </div>
         ${
           documents.length > 0
-            ? `<ul>${documents.map((document) => createDocument(document)).join("")}</ul>`
+            ? `<ul class ="banner ${active ? "active" : ""}">
+            ${documents.map((document) => createDocument(document)).join("")}</ul>`
             : ""
         }
       </li>`;
 };
 
-export const addDocument = (initialState, targetId, createdDocument) => {
+export const addDocument = (initialState, targetId = "", createdDocument) => {
   const nextState = JSON.parse(JSON.stringify(initialState));
+
+  if (targetId === null) {
+    nextState.push(createdDocument);
+    return nextState;
+  }
 
   const recur = (parentDocument) => {
     if (parentDocument.length === 0) {
@@ -24,12 +35,21 @@ export const addDocument = (initialState, targetId, createdDocument) => {
       for (const childDocument of parentDocument) {
         if (childDocument.id === +targetId) {
           childDocument.documents.push(createdDocument);
+
+          childDocument.active = true;
+          // if (childDocument.hasOwnProperty("active")) {
+
+          // } else {
+          //   childDocument.active = true;
+          // }
+
           return;
         }
         recur(childDocument.documents);
       }
     }
   };
+
   recur(nextState);
 
   return nextState;
@@ -38,7 +58,7 @@ export const addDocument = (initialState, targetId, createdDocument) => {
 export const deleteDocument = (initialState, targetId) => {
   const nextState = JSON.parse(JSON.stringify(initialState));
 
-  const recur = (parrentDocument) => {
+  const recur = (parrentDocument, parrent = null) => {
     if (parrentDocument.length === 0) {
       return;
     } else {
@@ -47,11 +67,14 @@ export const deleteDocument = (initialState, targetId) => {
           const temp = JSON.parse(JSON.stringify(childDocument.documents));
           const idx = parrentDocument.findIndex((document) => document.id === +targetId);
           parrentDocument.splice(idx, 1);
+
+          if (parrentDocument.length === 0) parrent.active = false;
+
           nextState.push(...temp);
           return;
         }
 
-        recur(childDocument.documents);
+        recur(childDocument.documents, childDocument);
       }
     }
   };
@@ -75,6 +98,33 @@ export const updateDocument = (initialState, { id, title, content }) => {
           return;
         }
 
+        recur(childDocument.documents);
+      }
+    }
+  };
+
+  recur(nextState);
+
+  return nextState;
+};
+
+export const toggleDocument = (initialState, targetId) => {
+  const nextState = JSON.parse(JSON.stringify(initialState));
+
+  const recur = (parentDocument) => {
+    if (parentDocument.length === 0) {
+      return;
+    } else {
+      for (const childDocument of parentDocument) {
+        if (childDocument.id === +targetId) {
+          if (childDocument.hasOwnProperty("active")) {
+            childDocument.active = !childDocument.active;
+          } else {
+            childDocument.active = true;
+          }
+
+          return;
+        }
         recur(childDocument.documents);
       }
     }
